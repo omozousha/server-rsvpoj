67e1a23eb10f7ed94d1d62ef506c1e8107cd1899
omozousha <hottiopan@raharja.info>
2024-02-23 01:29:19 +0700

    14

--- client/src/components/Rsvp.js
+++ client/src/components/Rsvp.js
@@ -1,60 +1,92 @@
 import React, { useState, useEffect } from 'react';
-import './Rsvp.css';
 
 function Rsvp() {
-    const [guestList, setGuestList] = useState([]);
 
-    useEffect(() => {
-        // Fetch data from the /guestlist endpoint
-        fetch('https://server-rsvpoj.vercel.app/api/guestlist')
-            .then(response => response.json())
-            .then(data => setGuestList(data))
-            .catch(error => console.error('Error fetching guest list:', error));
-    }, []);
+  const [guestList, setGuestList] = useState([]);
+  
+  const [formData, setFormData] = useState({
+    name: '',
+    attendance: '',
+    message: ''  
+  });
 
-    return (
-        <div>
-            <div className="form-container">
-                <div className="form">
-                    <h1>RSVP Form</h1>
-                    <form
-                        action="/"
-                        method="POST"
-                        onsubmit="event.preventDefault(); submitForm();"
-                    >
-                        <label htmlFor="name">Name:</label>
-                        <input
-                            placeholder="Your Name"
-                            type="text"
-                            id="name"
-                            name="name"
-                            className="input"
-                            required=""
-                        />
-                        <label htmlFor="attendance">Attendance:</label>
-                        <select id="attendance" name="attendance" className="select" required="">
-                            <option value="">-- Select Attendance --</option>
-                            <option value="datang">Datang</option>
-                            <option value="tidak datang">Tidak Datang</option>
-                        </select>
-                        <label htmlFor="message">Message:</label>
-                        <textarea
-                            placeholder="Say hello"
-                            id="message"
-                            name="message"
-                            className="textarea"
-                            required=""
-                            defaultValue={""}
-                        />
-                        <div className="button-container">
-                            <button type="submit" className="send-button">
-                                Submit
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-            <div>
+  useEffect(() => {
+    // Fetch guest list data
+  }, []);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  }
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    fetch('https://server-rsvpoj.vercel.app/api/guestlist', {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+    .then(response => {
+      setFormData({
+        name: '',
+        attendance: '',
+        message: ''
+      });
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  }
+
+  return (
+    <div>
+      <div className="form-container">
+        <div className="form">
+          <h1>RSVP Form</h1>
+          
+          <form onChange={handleChange} onSubmit={handleSubmit}>
+            <label htmlFor="name">Name:</label>
+            <input 
+              required
+              placeholder="Your Name"
+              type="text" 
+              id="name"
+              name="name"
+              value={formData.name}
+            />
+
+            <label htmlFor="attendance">Attendance:</label>
+            <select
+              required
+              id="attendance"
+              name="attendance"
+              value={formData.attendance}  
+            >
+              <option value="">-- Select Attendance --</option>
+              <option value="coming">Coming</option>
+              <option value="not_coming">Not Coming</option>
+            </select>
+
+            <label htmlFor="message">Message:</label>
+            <textarea
+              required
+              placeholder="Say hello"
+              id="message"
+              name="message"
+              value={formData.message}
+            />
+            
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </div>
+
+      <div>
                 <h2>Guest Response:</h2>
                 <div id="guestList" className="chatContainer">
                     {guestList.map(user => (
