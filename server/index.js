1db153ce599a4e64d8721a4b9b0b97e18edd632c
omozousha <hottiopan@raharja.info>
2024-02-23 00:57:11 +0700

    10

--- server/index.js
+++ server/index.js
@@ -1,19 +1,20 @@
-// /api/index.js
-const mongoose = require('mongoose');
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors"); // Import the cors module
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors()); // Use the cors middleware
 
-let cachedDb = null;
+// Koneksi ke MongoDB
+mongoose
+  .connect("process.env.MONGODB_URI")
+  .then(() => {
+    console.log("Koneksi sukses");
+  })
+  .catch((err) => console.log("Tidak dapat terhubung ke database"));
 
-async function connectToDatabase() {
-  if (cachedDb) {
-    return;
-  }
-  await mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  });
-  cachedDb = mongoose.connection;
-}
-
+// Membuat skema
 const userSchema = new mongoose.Schema({
   name: String,
   attendance: String,
@@ -23,18 +24,45 @@ const userSchema = new mongoose.Schema({
     default: Date.now
   }
 });
+
 const User = mongoose.model("User", userSchema);
 
-module.exports = async (req, res) => {
-  await connectToDatabase();
-  if (req.method === 'POST') {
-    let newUser = new User({
-      name: req.body.name,
-      attendance: req.body.attendance,
-      message: req.body.message
-    });
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "..client/src/App.js");
+});
 
-    await newUser.save();
-    res.redirect("/");
+app.post("/", function (req, res) {
+  let newUser = new User({
+    name: req.body.name,
+    attendance: req.body.attendance,
+    message: req.body.message
+  });
+
+  newUser.save();
+  res.redirect("/");
+});
+
+// Endpoint untuk menampilkan data dari database
+app.get("/guestlist", async function (req, res) {
+
+  try {
+
+    // Urutkan berdasarkan tanggal eventDate secara descending
+    const users = await User.find({}).sort({eventDate: -1}); 
+
+    res.send(users);
+
+  } catch (err) {
+
+    console.log("Error occurred:", err);
+
+    res.status(500).send("An error occurred while retrieving users");
+  
   }
-};
+
+});
+
+
+app.listen(5000, function () {
+  console.log("Server berjalan di port 5000");
+});
