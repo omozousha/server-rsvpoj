87deff90d73a1ef92a6769bec21e68384cc4916e
omozousha <hottiopan@raharja.info>
2024-02-23 13:30:10 +0700

    21

--- server/index.js
+++ server/index.js
@@ -8,7 +8,9 @@ app.use(cors()); // Use the cors middleware
 
 // Koneksi ke MongoDB
 mongoose
-  .connect("mongodb+srv://omozousha12:Ziggyss12@ojdatabase.mc8xxun.mongodb.net/TAMU-RSVP")
+  .connect(
+    "mongodb+srv://omozousha12:Ziggyss12@ojdatabase.mc8xxun.mongodb.net/TAMU-RSVP"
+  )
   .then(() => {
     console.log("Koneksi sukses");
   })
@@ -21,8 +23,8 @@ const userSchema = new mongoose.Schema({
   message: String,
   eventDate: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
 const User = mongoose.model("User", userSchema);
@@ -35,7 +37,7 @@ app.post("/", function (req, res) {
   let newUser = new User({
     name: req.body.name,
     attendance: req.body.attendance,
-    message: req.body.message
+    message: req.body.message,
   });
 
   newUser.save();
@@ -44,24 +46,36 @@ app.post("/", function (req, res) {
 
 // Endpoint untuk menampilkan data dari database
 app.get("/api/guestlist", async function (req, res) {
-
   try {
-
     // Urutkan berdasarkan tanggal eventDate secara descending
-    const users = await User.find({}).sort({eventDate: -1}); 
+    const users = await User.find({}).sort({ eventDate: -1 });
 
     res.send(users);
-
   } catch (err) {
-
     console.log("Error occurred:", err);
 
     res.status(500).send("An error occurred while retrieving users");
-  
   }
-
 });
 
+// Endpoint untuk menambahkan data ke database
+app.post("/api/guestlist", async function (req, res) {
+  try {
+    let newUser = new User({
+      name: req.body.name,
+      attendance: req.body.attendance,
+      message: req.body.message,
+    });
+
+    await newUser.save();
+
+    res.send("User added successfully");
+  } catch (err) {
+    console.log("Error occurred:", err);
+
+    res.status(500).send("An error occurred while adding user");
+  }
+});
 
 app.listen(5000, function () {
   console.log("Server berjalan di port 5000");
