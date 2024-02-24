704dffd289671b610e735a3cd216945b7087c3a9
omozousha <hottiopan@raharja.info>
2024-02-25 00:40:04 +0700

    hampir sempurna v2

--- client/src/components/Rsvp.js
+++ client/src/components/Rsvp.js
@@ -8,30 +8,38 @@ function Rsvp() {
     attendance: "",
     message: "",
   });
+  const [isLoading, setIsLoading] = useState(false); // New state for loading modal
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
   const handleSubmit = (e) => {
-  e.preventDefault();
+    e.preventDefault();
+    setIsLoading(true); // Show loading modal
 
-  fetch("https://server-rsvpoj.vercel.app/api/addRsvp", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(formData),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      setGuestList([...guestList, data]);
-      setFormData({ name: "", attendance: "", message: "" }); // Reset form after submission
+    fetch("https://server-rsvpoj.vercel.app/api/addRsvp", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
     })
-    .catch((error) => console.error("Error submitting form:", error));
-};
+      .then((response) => response.json())
+      .then((data) => {
+        setGuestList([...guestList, data]);
+        setFormData({ name: "", attendance: "", message: "" }); // Reset form after submission
+        setTimeout(() => {
+          setIsLoading(false); // Hide loading modal after 3 seconds
+          fetchGuestList(); // Refresh the guest list
+        }, 3000);
+      })
+      .catch((error) => {
+        console.error("Error submitting form:", error);
+        setIsLoading(false); // Hide loading modal if there's an error
+      });
+  };
 
-  // Fetch data from the /guestlist endpoint
   const fetchGuestList = () => {
     fetch("https://server-rsvpoj.vercel.app/guestlist")
       .then((response) => response.json())
@@ -45,6 +53,7 @@ function Rsvp() {
 
   return (
     <div>
+      {isLoading && <div>Loading...</div>} {/* Loading modal */}
       <div className="form-container">
         <div className="form">
           <h1>RSVP Form</h1>
