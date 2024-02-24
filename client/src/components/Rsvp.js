f1ce6d089d25add87c8c183e89b07973634e3065
omozousha <hottiopan@raharja.info>
2024-02-24 22:34:25 +0700

    35

--- client/src/components/Rsvp.js
+++ client/src/components/Rsvp.js
@@ -1,102 +1,115 @@
-import React, { useState, useEffect } from 'react';
-import './Rsvp.css';
+import React, { useState, useEffect } from "react";
+import "./Rsvp.css";
 
 function Rsvp() {
-    const [guestList, setGuestList] = useState([]);
-    const [formData, setFormData] = useState({
-        name: '',
-        attendance: '',
-        message: '',
-    });
+  const [guestList, setGuestList] = useState([]);
+  const [formData, setFormData] = useState({
+    name: "",
+    attendance: "",
+    message: "",
+  });
 
-    useEffect(() => {
-        // Fetch data from the /guestlist endpoint
-        fetch('https://server-rsvpoj.vercel.app/guestlist')
-            .then(response => response.json())
-            .then(data => setGuestList(data))
-            .catch(error => console.error('Error fetching guest list:', error));
-    }, []);
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
-    const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    fetch("https://server-rsvpoj.vercel.app/api/addRsvp", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setGuestList([...guestList, data]);
+        setFormData({ name: "", attendance: "", message: "" }); // Reset form after submission
+        fetchGuestList(); // Refresh guest list
+      })
+      .catch((error) => console.error("Error submitting form:", error));
+  };
 
-        // Send data to the server
-        fetch('https://server-rsvpoj.vercel.app/submit', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData),
-        })
-        .then(response => response.json())
-        .then(data => {
-            // Update the guest list with the new data
-            setGuestList([...guestList, data]);
-        })
-        .catch(error => console.error('Error submitting form:', error));
-    };
+  // Fetch data from the /guestlist endpoint
+  const fetchGuestList = () => {
+    fetch("https://server-rsvpoj.vercel.app/guestlist")
+      .then((response) => response.json())
+      .then((data) => setGuestList(data))
+      .catch((error) => console.error("Error fetching guest list:", error));
+  };
 
-    return (
-        <div>
-            <div className="form-container">
-                <div className="form">
-                    <h1>RSVP Form</h1>
-                    <form onSubmit={handleSubmit}>
-                        <label htmlFor="name">Name:</label>
-                        <input
-                            type="text"
-                            id="name"
-                            name="name"
-                            value={formData.name}
-                            onChange={handleChange}
-                            required
-                        />
-                        <label htmlFor="attendance">Attendance:</label>
-                        <select
-                            id="attendance"
-                            name="attendance"
-                            value={formData.attendance}
-                            onChange={handleChange}
-                            required
-                        >
-                            <option value="">-- Select Attendance --</option>
-                            <option value="datang">Datang</option>
-                            <option value="tidak datang">Tidak Datang</option>
-                        </select>
-                        <label htmlFor="message">Message:</label>
-                        <textarea
-                            id="message"
-                            name="message"
-                            value={formData.message}
-                            onChange={handleChange}
-                            required
-                        />
-                        <div className="button-container">
-                            <button type="submit" className="send-button">
-                                Submit
-                            </button>
-                        </div>
-                    </form>
-                </div>
+  useEffect(() => {
+    fetchGuestList();
+  }, []);
+
+  return (
+    <div>
+      <div className="form-container">
+        <div className="form">
+          <h1>RSVP Form</h1>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+            <label htmlFor="attendance">Attendance:</label>
+            <select
+              id="attendance"
+              name="attendance"
+              value={formData.attendance}
+              onChange={handleChange}
+              required
+            >
+              <option value="">-- Select Attendance --</option>
+              <option value="datang">Datang</option>
+              <option value="tidak datang">Tidak Datang</option>
+            </select>
+            <label htmlFor="message">Message:</label>
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              required
+            />
+            <div className="button-container">
+              <button type="submit" className="send-button">
+                Submit
+              </button>
             </div>
-            <div>
-                <h2>Guest Response:</h2>
-                <div id="guestList" className="chatContainer">
-                    {guestList.map(user => (
-                        <div key={user.name} className="chatBalloon">
-                            <p className='pesan'><strong></strong> {user.message}</p>
-                            <p className='nama'><strong>Name:</strong> {user.name}</p>
-                            <p className='tanggal'><strong>Date:</strong> {new Date(user.eventDate).toLocaleDateString()}</p>
-                        </div>
-                    )).sort((a,b) => new Date(b.eventDate) - new Date(a.eventDate))}
-                </div>
-            </div>
+          </form>
         </div>
-    );
+      </div>
+      <div>
+        <h2>Guest Response:</h2>
+        <div id="guestList" className="chatContainer">
+          {guestList
+            .map((user) => (
+              <div key={user.name} className="chatBalloon">
+                <p className="pesan">
+                  <strong></strong> {user.message}
+                </p>
+                <p className="nama">
+                  <strong>Name:</strong> {user.name}
+                </p>
+                <p className="tanggal">
+                  <strong>Date:</strong>{" "}
+                  {new Date(user.eventDate).toLocaleDateString()}
+                </p>
+              </div>
+            ))
+            .sort((a, b) => new Date(b.eventDate) - new Date(a.eventDate))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Rsvp;
--- server/index.js
+++ server/index.js
@@ -2,15 +2,14 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const cors = require("cors"); // Import the cors module
+const cors = require("cors");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors()); // Use the cors middleware
+app.use(express.json()); // Middleware untuk mengurai body dalam format JSON
+app.use(cors());
 
 // Koneksi ke MongoDB
 mongoose
-  .connect(
-    "mongodb+srv://omozousha12:Ziggyss12@ojdatabase.mc8xxun.mongodb.net/TAMU-RSVP"
-  )
+  .connect("mongodb+srv://omozousha12:Ziggyss12@ojdatabase.mc8xxun.mongodb.net/TAMU-RSVP")
   .then(() => {
     console.log("Koneksi sukses");
   })
@@ -29,58 +28,37 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", userSchema);
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "..client/src/App.js");
-});
-
-app.post("/", async function (req, res) {
+// Endpoint untuk menambahkan RSVP ke database
+app.post("/api/addRsvp", async function (req, res) {
   try {
-    let newUser = new User({
+    let newRsvp = new User({
       name: req.body.name,
       attendance: req.body.attendance,
       message: req.body.message,
     });
 
-    await newUser.save();
+    await newRsvp.save();
 
-    res.send("User added successfully");
+    // Kirim respons dalam format JSON yang valid
+    res.status(201).json({
+      name: newRsvp.name,
+      attendance: newRsvp.attendance,
+      message: newRsvp.message,
+    });
   } catch (err) {
     console.log("Error occurred:", err);
-
-    res.status(500).send("An error occurred while adding user");
+    res.status(500).send("An error occurred while adding RSVP");
   }
 });
 
 // Endpoint untuk menampilkan data dari database
 app.get("/guestlist", async function (req, res) {
   try {
-    // Urutkan berdasarkan tanggal eventDate secara descending
     const users = await User.find({}).sort({ eventDate: -1 });
-
     res.send(users);
   } catch (err) {
     console.log("Error occurred:", err);
-
     res.status(500).send("An error occurred while retrieving users");
-  }
-});
-
-// Endpoint untuk menambahkan data ke database
-app.post("/guestlist", async function (req, res) {
-  try {
-    let newUser = new User({
-      name: req.body.name,
-      attendance: req.body.attendance,
-      message: req.body.message,
-    });
-
-    await newUser.save();
-
-    res.send("User added successfully");
-  } catch (err) {
-    console.log("Error occurred:", err);
-
-    res.status(500).send("An error occurred while adding user");
   }
 });
 
