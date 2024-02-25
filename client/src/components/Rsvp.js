748283da6b5401285311f7a07acb4f124d4f8f8c
omozousha <hottiopan@raharja.info>
2024-02-25 21:52:33 +0700

    add countdown rsvplist update

--- client/src/components/Rsvp.js
+++ client/src/components/Rsvp.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Modal from "react-modal";
 import "./Rsvp.css";
 
-Modal.setAppElement("#root"); // This line is needed for accessibility reasons
+Modal.setAppElement("#root");
 
 function Rsvp() {
   const [guestList, setGuestList] = useState([]);
@@ -11,15 +11,27 @@ function Rsvp() {
     attendance: "",
     message: "",
   });
-  const [modalIsOpen, setModalIsOpen] = useState(false); // New state for modal
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [countdown, setCountdown] = useState(3);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
+  const startCountdown = () => {
+    const intervalId = setInterval(() => {
+      setCountdown((prevCountdown) => prevCountdown - 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+      setCountdown(3);
+    };
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    setModalIsOpen(true); // Open modal
+    setModalIsOpen(true);
 
     fetch("https://server-rsvpoj.vercel.app/api/addRsvp", {
       method: "POST",
@@ -31,15 +43,18 @@ function Rsvp() {
       .then((response) => response.json())
       .then((data) => {
         setGuestList([...guestList, data]);
-        setFormData({ name: "", attendance: "", message: "" }); // Reset form after submission
+        setFormData({ name: "", attendance: "", message: "" });
+
+        startCountdown();
+
         setTimeout(() => {
-          setModalIsOpen(false); // Close modal after 3 seconds
-          fetchGuestList(); // Refresh the guest list data
+          setModalIsOpen(false);
+          fetchGuestList();
         }, 3000);
       })
       .catch((error) => {
         console.error("Error submitting form:", error);
-        setModalIsOpen(false); // Close modal if there's an error
+        setModalIsOpen(false);
       });
   };
 
@@ -56,25 +71,29 @@ function Rsvp() {
 
   return (
     <div>
-      <Modal 
-  isOpen={modalIsOpen} 
-  style={{
-    overlay: {
-      display: 'flex',
-      alignItems: 'center', // This will align the modal vertically in the center
-      justifyContent: 'center', // This will align the modal horizontally in the center
-      backgroundColor: 'rgba(0, 0, 0, 0.75)'
-    },
-    content: {
-      color: 'lightsteelblue',
-      width: '10%', // Change the width of the modal
-      height: '5%', // Change the height of the modal
-      transition: 'all 0.3s ease-in-out' // Add a transition animation
-    }
-  }}
->
-  Loading...
-</Modal>
+      <Modal
+        isOpen={modalIsOpen}
+        style={{
+          overlay: {
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "rgba(0, 0, 0, 0.75)",
+          },
+          content: {
+            color: "lightsteelblue",
+            width: "10%",
+            height: "5%",
+            transition: "all 0.3s ease-in-out",
+          },
+        }}
+      >
+        {countdown > 0 ? (
+          <p>Modal will close in {countdown} seconds...</p>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </Modal>
       <div className="form-container">
         <div className="form">
           <h1>RSVP Form</h1>
