8c62156442a84de5bb3627119f5cc7d257eb4075
omozousha <hottiopan@raharja.info>
2024-02-26 00:42:03 +0700

    add Rsvpmasterv6

--- client/src/components/Rsvpmaster.js
+++ client/src/components/Rsvpmaster.js
@@ -9,6 +9,11 @@ const Rsvpmaster = () => {
   const [guestList, setGuestList] = useState([]);
 
   const handleSubmit = async () => {
+    if (!name || !attendance) {
+      alert("Please fill in your name and attendance before submitting");
+      return;
+    }
+
     try {
       await axios.post("https://server-rsvpoj.vercel.app/api/addRsvp", {
         name,
@@ -25,27 +30,12 @@ const Rsvpmaster = () => {
 
   const fetchGuestList = async () => {
     try {
-      const response = await axios.get("https://server-rsvpoj.vercel.app/guestlist");
+      const response = await axios.get(
+        "https://server-rsvpoj.vercel.app/guestlist"
+      );
       setGuestList(response.data);
     } catch (error) {
       console.error("Error fetching guest list:", error);
-    }
-  };
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    switch (name) {
-      case "name":
-        setName(value);
-        break;
-      case "attendance":
-        setAttendance(value);
-        break;
-      case "message":
-        setMessage(value);
-        break;
-      default:
-        break;
     }
   };
 
@@ -57,42 +47,62 @@ const Rsvpmaster = () => {
   return (
     <div className="form-container">
       <div className="form">
-      <h2>RSVP Form</h2>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <label>Name:</label>
-        <input type="text" name="name" value={name} onChange={handleChange} required placeholder="Enter your name" />
+        <h2>RSVP Form</h2>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <label>Name:</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+            placeholder="Enter your name"
+          />
 
-        <label>Attendance:</label>
-        <select name="attendance" value={attendance} onChange={handleChange} required>
-          <option value="">Select</option>
-          <option value="Attending">Attending</option>
-          <option value="Not Attending">Not Attending</option>
-        </select>
+          <label>Attendance:</label>
+          <select
+            value={attendance}
+            onChange={(e) => setAttendance(e.target.value)}
+            required
+          >
+            <option value="">Select</option>
+            <option value="Attending">Attending</option>
+            <option value="Not Attending">Not Attending</option>
+          </select>
 
-        <label>Message:</label>
-        <textarea name="message" value={message} onChange={handleChange} placeholder="Enter your message"></textarea>
+          <label>Message:</label>
+          <textarea
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder="Enter your message"
+          ></textarea>
 
-        <div className="button-container">
-        <button onClick={handleSubmit} className="send-button">Submit RSVP</button>
-        </div>
-      </form>
-
-      <h2>Guest List</h2>
-      <div id="guestList" className="chatContainer">
-        {guestList.map((guest) => (
-          <div key={guest._id} className="chatBalloon">
-            <p className="pesan">
-              <strong></strong>"{guest.message}"</p>
-            <p className="nama">
-              <strong>Name:</strong> {guest.name}</p>
-            <p className="kehadiran">
-              <strong>Attendance:</strong> {guest.attendance}</p>
-            <p className="tanggal">
-              {new Date(guest.eventDate).toLocaleDateString()}</p>
+          <div className="button-container">
+            <button onClick={handleSubmit} className="send-button">
+              Submit RSVP
+            </button>
           </div>
-        ))}
+        </form>
+
+        <h2>Guest List</h2>
+        <div id="guestList" className="chatContainer">
+          {guestList.map((guest) => (
+            <div key={guest._id} className="chatBalloon">
+              <p className="pesan">
+                <strong></strong>"{guest.message}"
+              </p>
+              <p className="nama">
+                <strong>Name:</strong> {guest.name}
+              </p>
+              <p className="kehadiran">
+                <strong>Attendance:</strong> {guest.attendance}
+              </p>
+              <p className="tanggal">
+                {new Date(guest.eventDate).toLocaleDateString()}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
     </div>
   );
 };
